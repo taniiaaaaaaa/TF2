7c99f9849b2353577d0ad092f763c11105bda5bb
taniiaaaaaaa <158506158+taniiaaaaaaa@users.noreply.github.com>
2024-06-25 23:23:16 -0600

    Update tf2.py

--- tf2.py
+++ tf2.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1yEJV3jUgZyk6AttpkMAisbQHLWcgZoKJ
 """
 
-pip install streamlit
+#pip install streamlit
 
 import streamlit as st
 import yfinance as yf
